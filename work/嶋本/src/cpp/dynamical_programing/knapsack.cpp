924b912cb5a273700328cb2036d0f627b0cd1cf4
shimamon <gitworkshimaon2020@gmail.com>
2022-06-04 21:23:03 +0900

    ・ファイル整理　・2022/06/04資料追加

--- work/嶋本/src/cpp/dynamical_programing/knapsack.cpp
+++ work/嶋本/src/cpp/dynamical_programing/knapsack.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 // 比較して最大の値に更新
 template <class T>
-void chmax(T &a, T b) {
+void chMax(T &a, T b) {
     if (a < b) {
         a = b;
     }
@@ -16,22 +16,24 @@ struct Goods {
 };
 
 long long solveKnapsackProblem(vector<Goods> &goodsList, long long W,
-                               vector<vector<long long>> &dp, vector<vector<long long>> &prevW) {
+                               vector<vector<long long>> &dp,
+                               vector<vector<long long>> &prevW) {
     int N = goodsList.size();
     for (int i = 0; i < N; ++i) {       // 品物についてのループ
         for (int w = 0; w <= W; ++w) {  // 重量についてのループ
-        
+
             // i番目の品物を選ぶとき
             if (w >= goodsList[i].weight) {
-
                 // 個数制限があるとき
-                long long newElem = dp[i][w - goodsList[i].weight] + goodsList[i].value;
+                long long update =
+                    dp[i][w - goodsList[i].weight] + goodsList[i].value;
 
                 // 個数制限がないとき
-                // long long newElem = dp[i + 1][w - goodsList[i].weight] + goodsList[i].value;
+                // long long update = dp[i + 1][w - goodsList[i].weight] +
+                // goodsList[i].value;
 
-                if (dp[i + 1][w] < newElem) {
-                    dp[i + 1][w] = newElem;
+                if (dp[i + 1][w] < update) {
+                    dp[i + 1][w] = update;
                     prevW[i + 1][w] = w - goodsList[i].weight;
                 }
             }
@@ -41,11 +43,25 @@ long long solveKnapsackProblem(vector<Goods> &goodsList, long long W,
                 dp[i + 1][w] = dp[i][w];
                 prevW[i + 1][w] = w;
             }
-
         }
     }
 
     return dp[N][W];
+}
+
+void showCombination(int W, vector<vector<long long>> &prevW,
+                vector<Goods> &goodsList) {
+    int curWeight = W;
+    for (int i = goodsList.size() - 1; i >= 0; --i) {
+        // i番目の品物を選んでいた場合
+        if (prevW[i + 1][curWeight] == curWeight - goodsList[i].weight) {
+            cout << i + 1 << "番目, "
+                 << "Weight: " << goodsList[i].weight
+                 << ", value: " << goodsList[i].value << endl;
+        }
+
+        curWeight = prevW[i + 1][curWeight];
+    }
 }
 
 int main() {
@@ -53,7 +69,8 @@ int main() {
     cout << "Max Weight: ";
     cin >> W;
 
-    vector<Goods> goodsList = {{2, 3}, {1, 2}, {3, 6}, {2, 1}, {1, 3}, {5, 85}};
+    vector<Goods> goodsList = {{1, 3}, {1, 3}, {3, 5}, {3, 5}, {3, 5},
+                               {3, 5}, {4, 8}};
 
     int N = goodsList.size();
 
@@ -68,17 +85,8 @@ int main() {
     // 結果出力処理
     cout << "Max Value: " << res << endl;
 
-
     // 組み合わせ出力処理
-    int curWeight = W;
-    for (int i = N - 1; i >= 0; --i) {
-        // i番目の品物を選んでいた場合
-        if (prevW[i + 1][curWeight] == curWeight - goodsList[i].weight) {
-            cout << i + 1 << "番目, " << "Weight: " << goodsList[i].weight << ", value: " << goodsList[i].value << endl;
-        }
-
-        curWeight = prevW[i + 1][curWeight];
-    }
+    showCombination(W, prevW, goodsList);
 
     return 0;
 }
