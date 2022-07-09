ecab6e30250c12a91b0cc8a66f237aca6f9a6130
soulburner <105105640+yuktanaka@users.noreply.github.com>
2022-07-09 20:02:08 +0900

    Add files via upload

---
+++ 田中/Java/shooting2/src/main/java/io/github/kusaanko/Shooting/Shooting.java
@@ -0,0 +1,9 @@
+package io.github.kusaanko.Shooting;
+
+public class Shooting {
+    public static ShootingFrame hootingFrame;
+    public static void main(String[] args) {
+       shootingFrame = new ShootingFrame();
+
+    }
+}
