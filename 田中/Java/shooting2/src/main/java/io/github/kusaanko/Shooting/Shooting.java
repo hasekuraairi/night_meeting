e8e566b541a6519d8adfbade224a4d9a5f23efee
soulburner <105105640+yuktanaka@users.noreply.github.com>
2022-07-09 20:02:28 +0900

    Delete 田中 directory

--- 田中/Java/shooting2/src/main/java/io/github/kusaanko/Shooting/Shooting.java
+++
@@ -1,9 +0,0 @@
-package io.github.kusaanko.Shooting;
-
-public class Shooting {
-    public static ShootingFrame hootingFrame;
-    public static void main(String[] args) {
-       shootingFrame = new ShootingFrame();
-
-    }
-}
